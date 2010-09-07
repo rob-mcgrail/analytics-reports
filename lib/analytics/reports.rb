9d7bf72448f2a06fff9e923622dcf867b137b177
robomc <rob.mcgrail@gmail.com>
2010-09-07 15:07:16 +1200

    removed reports

--- lib/analytics/reports.rb
+++ lib/analytics/reports.rb
@@ -1,20 +1,2 @@
 #parent
 require File.expand_path(File.dirname(__FILE__) + "/reports/report.rb")
-
-#children
-require File.expand_path(File.dirname(__FILE__) + "/reports/prototype.rb")
-
-#community usage stats grab
-require File.expand_path(File.dirname(__FILE__) + "/reports/community_usage.rb")
-
-#cmis stats grab
-require File.expand_path(File.dirname(__FILE__) + "/reports/cmis.rb")
-
-#big reports
-require File.expand_path(File.dirname(__FILE__) + "/reports/sciencelearn.rb")
-require File.expand_path(File.dirname(__FILE__) + "/reports/biotech.rb")
-
-#a check for analytics
-require File.expand_path(File.dirname(__FILE__) + "/reports/check.rb") #combined
-require File.expand_path(File.dirname(__FILE__) + "/reports/tki_check.rb")
-require File.expand_path(File.dirname(__FILE__) + "/reports/cwa_check.rb")
