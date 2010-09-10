e96573c75b38b0fd061bbd6421c1b7531c6fbc5d
rob <rob@rob-laptop.(none)>
2010-09-10 22:45:10 +1200

    setting up the class (inherits most functionality from XML)

--- lib/analytics/format/fop.rb
+++ lib/analytics/format/fop.rb
@@ -1,4 +1,18 @@
 class fop < XML
 
+  def bar_series(struct)
+        #gets a url for a bargraph from the google charts api
+        #expects an ostruct containing:
+
+        # title, series (an array of values)
+    puts "this is the bar series method from fop"
+
+  end
+
+  def finish
+    $display.tell_user "Putting fop-ready xml output in to $collector. Access with $collector.fop"
+    $collector.fop = @collector
+  end
+
 end
 
