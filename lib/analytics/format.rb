705e1e85ab6a524fe7fce2f23e3eb0a9afef6a39
rob <rob@rob-laptop.(none)>
2010-09-10 21:42:30 +1200

    Added in gchart.rb require

--- lib/analytics/format.rb
+++ lib/analytics/format.rb
@@ -1,2 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + "/format/format.rb")
+require File.expand_path(File.dirname(__FILE__) + "/format/gchart.rb") #module
+
+require File.expand_path(File.dirname(__FILE__) + "/format/format.rb") #parent
+
 require File.expand_path(File.dirname(__FILE__) + "/format/xml.rb")
+
