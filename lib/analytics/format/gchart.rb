05cf73d019451cbb8a0ab98d231fe9fd2fd33665
rob <rob@rob-laptop.(none)>
2010-09-11 00:17:30 +1200

    finished off the bar_series method

--- lib/analytics/format/gchart.rb
+++ lib/analytics/format/gchart.rb
@@ -1,17 +1,22 @@
 module GoogleChart
-  require 'gchart'
+#  require 'gchart'
+#it hasn't worked for some of these, so I made them manually...'
 
-  def GoogleChart.bar_series(title, data, color = "BBCCED") #data is an array
+#
+# A module for getting google chart api data data
+#
+
+  def GoogleChart.bar_series(title, data, color = "BBCCED", size = "700x85") #data is an array
 
     data = self.to_param(data)
-    title = self.to_param(data)
+    title = self.to_param(title)
 
     src = ["http://chart.apis.google.com/chart",
           "?chxr=0,0,900",
           "&chxs=0,676767,8.5,0,l,676767",
           "&chxt=y",
           "&chbh=a,5",
-          "&chs=700x85",
+          "&chs=#{size}",
           "&cht=bvg",
           "&chco=#{color}",
           "&chds=0,1037",
@@ -21,6 +26,7 @@ module GoogleChart
           ]
 
     src = self.to_url(src)
+
     src
   end
 
@@ -38,20 +44,16 @@ module GoogleChart
     if thing.is_a? String #format titles for parameter
       string = thing.gsub(" ", "+")
     end
-
     string
   end
 
   def self.to_url(array) #takes the array of parameters and adds together in to a string
                     #there's probably an array method already that does this'
     s = String.new
-    array.each {|thing| s + thing}
+    array.each {|thing| s << thing}
     s
   end
 
-#
-# A module for getting google chart api data data
-#
 
 
 end
