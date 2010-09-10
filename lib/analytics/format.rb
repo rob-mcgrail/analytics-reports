f42f5a30a6dedb6e928ca6a1da2c9b0f40974b35
rob <rob@rob-laptop.(none)>
2010-09-10 22:46:53 +1200

    added in require fop.rb

--- lib/analytics/format.rb
+++ lib/analytics/format.rb
@@ -3,4 +3,5 @@ require File.expand_path(File.dirname(__FILE__) + "/format/gchart.rb") #module
 require File.expand_path(File.dirname(__FILE__) + "/format/format.rb") #parent
 
 require File.expand_path(File.dirname(__FILE__) + "/format/xml.rb")
+require File.expand_path(File.dirname(__FILE__) + "/format/fop.rb") #inherits from XML
 
