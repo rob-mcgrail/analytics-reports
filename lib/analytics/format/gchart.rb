ee28923900e5eb3d5de21cc400d7edf87367f2f8
rob <rob@rob-laptop.(none)>
2010-09-10 23:00:49 +1200

    added in test of the gchart gem with a call for a bar graph

--- lib/analytics/format/gchart.rb
+++ lib/analytics/format/gchart.rb
@@ -2,6 +2,11 @@ module GoogleChart
 
 require 'gchart'
 
+  def bar_series(struct)
+    a = struct.data[0]
+    src = Gchart.bar(:title => struct.title, :data => a)
+    src
+  end
 
 #
 # A module for getting google chart api data data
