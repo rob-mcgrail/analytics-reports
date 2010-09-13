bf1f1414233b5a00a5796290326bc0f3f7e53aad
rob <rob@rob-laptop.(none)>
2010-09-13 20:50:50 +1200

    added basic print method

--- lib/analytics/display/display.rb
+++ lib/analytics/display/display.rb
@@ -1,33 +1,38 @@
 class Display
   #Display includes methods for displaying results
   attr_reader :line_break
-  
+
   def initialize(line_break)
-    
+
     @line_break = line_break
   end
-  
+
   def break
     puts @line_break
   end
-  
+
   def print(thing)
     puts thing
   end
-  
+
   def ask_user(thing)
     puts thing
   end
-  
+
   def tell_user(thing)
     puts thing
   end
-  
+
   def alert_user(thing)
     puts thing
   end
-  
+
   def get_private(thing, ast = '*')
     raise "I am supposed to be obscuring a password, but you need a child of me to do this."
   end
+
+  def <<(thing)
+    puts thing
+  end
 end
+
--- lib/analytics/display/unix.rb
+++ lib/analytics/display/unix.rb
@@ -7,15 +7,15 @@ class Unix < Display
   def ask_user(thing)
     say(%{<%= color('#{thing}', GREEN)%>\n})
   end
-  
+
   def tell_user(thing)
     say(%{<%= color('#{thing}', YELLOW)%>\n})
   end
-  
+
   def alert_user(thing)
     say(%{<%= color('#{thing}', RED)%>\n})
   end
-  
+
   def get_private(thing, ast = '*')
     ask(%{<%= color('#{thing}', GREEN)%>\n}) { |pass| pass.echo="#{ast}" }
   end
