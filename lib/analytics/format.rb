c170bd7fc60dcefb786838e5fae38a73c86aeea1
robomc <rob.mcgrail@gmail.com>
2010-09-17 17:04:56 +1200

    adding requires for wicked report

--- lib/analytics/format.rb
+++ lib/analytics/format.rb
@@ -4,4 +4,5 @@ require File.expand_path(File.dirname(__FILE__) + "/format/format.rb") #parent
 
 require File.expand_path(File.dirname(__FILE__) + "/format/xml.rb")
 require File.expand_path(File.dirname(__FILE__) + "/format/fop.rb") #inherits from XML
+require File.expand_path(File.dirname(__FILE__) + "/format/txt.rb")
 
--- lib/analytics/reports.rb
+++ lib/analytics/reports.rb
@@ -1,3 +1,4 @@
 #parent
 require File.expand_path(File.dirname(__FILE__) + "/reports/report.rb")
 require File.expand_path(File.dirname(__FILE__) + "/reports/biotech.rb")
+require File.expand_path(File.dirname(__FILE__) + "/reports/wicked_month.rb")
