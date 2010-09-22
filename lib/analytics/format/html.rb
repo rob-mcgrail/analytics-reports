d2a46750b14bf0be70b47264b6195cd76e7dc2ec
robomc <rob.mcgrail@gmail.com>
2010-09-23 10:18:34 +1200

    built html table method

--- lib/analytics/format/html.rb
+++ lib/analytics/format/html.rb
@@ -97,22 +97,33 @@ class HTML
     #prints a table with title and headings
     #expects a ostruct with :title, :header (an array of heading titles)
     #and :rows, an array of arrays of data.
-
-    
-    @x.tag!(struct.title){
-      struct.rows.each do |row|
-        i = 1
-        n = row.length
-        @x.tag!(struct.header[0]){
-          @x.tag!("#{struct.header[0]}_name", row[0])
-          while n > 1
-            @x.tag!(struct.header[i], row[i])
-            i+=1
-            n-=1
+ 
+    @x.div("id" => "table"){
+      @x.table("id" => "#{struct.title}"){
+          if struct.header != nil
+          
+            if struct.header.length != struct.rows[0].length
+              raise "Different ammount of table column labels from table columns..."
+            end
+          
+            @x.tr{
+              struct.header.each do |header|
+                @x.th("#{header}")
+              end
+            }
+            
           end
-        }
-      end
-      }  
+          
+        struct.rows.each do |row|
+          @x.tr{
+            row.each do |cell|
+              @x.td("#{cell}")
+            end
+          }
+        end
+      } 
+    }
+    
   end
 
   def block_full(struct)
