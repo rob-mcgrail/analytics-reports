358013a2002aaaf9f66bd6f47076f722ecea1cce
robomc <rob.mcgrail@gmail.com>
2010-09-23 11:25:23 +1200

    rejiggering report for html

--- lib/analytics/reports/biotech.rb
+++ lib/analytics/reports/biotech.rb
@@ -8,7 +8,7 @@ class Biotech < Report
     @dir = "#{DateTime.now.strftime("%d%m%M%S")}"
     @path = File.expand_path(File.dirname(__FILE__) + "/../../../output/#{@dir}")
     FileUtils.mkdir_p @path
-    @file = File.new(@path + "/#{@name.gsub(" ", "-").downcase}-#{@now.strftime("%d%m")}.xml",  "w+")
+    @file = File.new(@path + "/#{@name.gsub(" ", "-").downcase}-#{@now.strftime("%d%m")}.html",  "w+")
     $path = @path #export path variable for formatting classes that need it
   end
 
@@ -52,135 +52,138 @@ class Biotech < Report
     countries = CountrySource.new
     new_returning = NewVisits.new
     
-    
-    $format.x.tag!(title){
-      $format.x.comment!("Header!")
-      $format.title("Sciencelearn.org.nz", "Traffic summary")
-      $format.date_section
+    $format.x.html{ 
+      $format.stylesheet
+      $format.x.body{  
+        $format.x.div("id"=>"page"){
+          $format.x.comment!("Header!")
+          $format.title("Sciencelearn.org.nz", "Traffic summary")
+          $format.date_section
 
 
 
-        $format.x.comment!("Main body!")
+            $format.x.comment!("Main body!")
+
+            $format.main_graph(visits.main_graph)
+
+            $format.table(traffic_sources.processed_reporting_bounce_and_time(7))
+
+            $format.table(countries.processed_reporting(7))
+
+            $format.bar_series(visits.hours_graph)
+
+            $format.x.comment!("Right hand section!")
+
+            $format.block_full(visits.three_with_changes) #blocks
+            $format.block_full(uniques.three_monthly_averages)
+            $format.block_full(bounces.three_with_changes)
+            $format.block_full(times.three_with_changes_as_averages)
+            $format.block_full(pages_visits.three_with_changes_per_visit)
 
 
-        $format.main_graph(visits.main_graph)
-
-        $format.table(traffic_sources.processed_reporting_bounce_and_time(7))
-
-        $format.table(countries.processed_reporting(7))
-
-        $format.bar_series(visits.hours_graph)
-
-        $format.x.comment!("Right hand section!")
-
-        $format.block_full(visits.three_with_changes) #blocks
-        $format.block_full(uniques.three_monthly_averages)
-        $format.block_full(bounces.three_with_changes)
-        $format.block_full(times.three_with_changes_as_averages)
-        $format.block_full(pages_visits.three_with_changes_per_visit)
-
-
-        $format.comparison(new_returning.reporting_only)      #bar
+            $format.comparison(new_returning.reporting_only)      #bar
+        }
+      }
     }
   end
   
-  def country(page = "-", segment = nil, segment_string = nil, modifier = nil)
-    
-    $profile.segment = segment
-    $profile.segment_string = segment_string
-    
-    visits = Visits.new
-    uniques = Uniques.new
-    bounces = BounceRate.new
-    times = Times.new
-    pages_visits = PageViews.new
-    new_returning = NewVisits.new
-    traffic_sources = WebSources.new
-    engagement_pages = Content.new
-    
-    $format.x.tag!(page){
-      $format.x.comment!("Header!")
-      $format.title(page, "By Country")
-      $format.date_section
-      
-        $format.x.comment!("Main body!")
-
-
-        $format.main_graph(visits.main_graph)
-        $format.bar_series(visits.hours_graph)
-        
-        $format.table(traffic_sources.processed_reporting_bounce_and_time(5))
-        $format.table(engagement_pages.ordered_by_pageviews($periods.start_date_reporting, $periods.end_date_reporting, 8))
-        $format.table(engagement_pages.ordered_by_time($periods.start_date_reporting, $periods.end_date_reporting, 8))
-        
-        $format.x.comment!("Right hand section!")
-
-        $format.block_full(visits.three_with_changes) #blocks
-        $format.block_full(uniques.three_monthly_averages)
-        if modifier != "b"
-          $format.block_full(bounces.three_with_changes)
-        end        
-        $format.block_full(times.three_with_changes_as_averages)
-        $format.block_full(pages_visits.three_with_changes_per_visit)
-
-        $format.comparison(new_returning.reporting_only)      #bar
-         
-    }
-    
-    $profile.segment = nil #reset
-    $profile.segment_string = nil #reset
-    
-  end
-  
-  def country_no_bounce(page = "-", segment = nil, segment_string = nil)
-    self.country(page, segment, segment_string, "b")
-  end
-  
-  def visitor_type(page = "-", segment = nil, segment_string = nil)   #make me work with modifier
-    
-    $profile.segment = segment
-    $profile.segment_string = segment_string
-    
-    visits = Visits.new
-    uniques = Uniques.new
-    bounces = BounceRate.new
-    times = Times.new
-    pages_visits = PageViews.new
-    new_returning = NewVisits.new
-    traffic_sources = WebSources.new
-    engagement_pages = Content.new
-    depth = Depth.new
-    length = Length.new
-    
-    $format.x.tag!(page){
-      $format.x.comment!("Header!")
-      $format.title(page, "Visitor type")
-      $format.date_section
-      
-        $format.x.comment!("Main body!")
-
-
-        $format.main_graph(visits.main_graph)
-        
-        $format.table(traffic_sources.processed_reporting_bounce_and_time(5))
-        
-        $format.labelled_series(depth.basic_five)
-        $format.labelled_series(length.basic_five)
-        
-        $format.table(engagement_pages.ordered_by_pageviews($periods.start_date_reporting, $periods.end_date_reporting, 8))
-        $format.table(engagement_pages.ordered_by_time($periods.start_date_reporting, $periods.end_date_reporting, 8))
-        
-        $format.x.comment!("Right hand section!")
-
-        $format.block_full(visits.three_with_changes) #blocks
-        $format.block_full(bounces.three_with_changes)
-
-    }
-    
-    $profile.segment = nil #reset
-    $profile.segment_string = nil #reset
-    
-  end
+  # def country(page = "-", segment = nil, segment_string = nil, modifier = nil)
+  #   
+  #   $profile.segment = segment
+  #   $profile.segment_string = segment_string
+  #   
+  #   visits = Visits.new
+  #   uniques = Uniques.new
+  #   bounces = BounceRate.new
+  #   times = Times.new
+  #   pages_visits = PageViews.new
+  #   new_returning = NewVisits.new
+  #   traffic_sources = WebSources.new
+  #   engagement_pages = Content.new
+  #   
+  #   $format.x.tag!(page){
+  #     $format.x.comment!("Header!")
+  #     $format.title(page, "By Country")
+  #     $format.date_section
+  #     
+  #       $format.x.comment!("Main body!")
+  # 
+  # 
+  #       $format.main_graph(visits.main_graph)
+  #       $format.bar_series(visits.hours_graph)
+  #       
+  #       $format.table(traffic_sources.processed_reporting_bounce_and_time(5))
+  #       $format.table(engagement_pages.ordered_by_pageviews($periods.start_date_reporting, $periods.end_date_reporting, 8))
+  #       $format.table(engagement_pages.ordered_by_time($periods.start_date_reporting, $periods.end_date_reporting, 8))
+  #       
+  #       $format.x.comment!("Right hand section!")
+  # 
+  #       $format.block_full(visits.three_with_changes) #blocks
+  #       $format.block_full(uniques.three_monthly_averages)
+  #       if modifier != "b"
+  #         $format.block_full(bounces.three_with_changes)
+  #       end        
+  #       $format.block_full(times.three_with_changes_as_averages)
+  #       $format.block_full(pages_visits.three_with_changes_per_visit)
+  # 
+  #       $format.comparison(new_returning.reporting_only)      #bar
+  #        
+  #   }
+  #   
+  #   $profile.segment = nil #reset
+  #   $profile.segment_string = nil #reset
+  #   
+  # end
+  # 
+  # def country_no_bounce(page = "-", segment = nil, segment_string = nil)
+  #   self.country(page, segment, segment_string, "b")
+  # end
+  # 
+  # def visitor_type(page = "-", segment = nil, segment_string = nil)   #make me work with modifier
+  #   
+  #   $profile.segment = segment
+  #   $profile.segment_string = segment_string
+  #   
+  #   visits = Visits.new
+  #   uniques = Uniques.new
+  #   bounces = BounceRate.new
+  #   times = Times.new
+  #   pages_visits = PageViews.new
+  #   new_returning = NewVisits.new
+  #   traffic_sources = WebSources.new
+  #   engagement_pages = Content.new
+  #   depth = Depth.new
+  #   length = Length.new
+  #   
+  #   $format.x.tag!(page){
+  #     $format.x.comment!("Header!")
+  #     $format.title(page, "Visitor type")
+  #     $format.date_section
+  #     
+  #       $format.x.comment!("Main body!")
+  # 
+  # 
+  #       $format.main_graph(visits.main_graph)
+  #       
+  #       $format.table(traffic_sources.processed_reporting_bounce_and_time(5))
+  #       
+  #       $format.labelled_series(depth.basic_five)
+  #       $format.labelled_series(length.basic_five)
+  #       
+  #       $format.table(engagement_pages.ordered_by_pageviews($periods.start_date_reporting, $periods.end_date_reporting, 8))
+  #       $format.table(engagement_pages.ordered_by_time($periods.start_date_reporting, $periods.end_date_reporting, 8))
+  #       
+  #       $format.x.comment!("Right hand section!")
+  # 
+  #       $format.block_full(visits.three_with_changes) #blocks
+  #       $format.block_full(bounces.three_with_changes)
+  # 
+  #   }
+  #   
+  #   $profile.segment = nil #reset
+  #   $profile.segment_string = nil #reset
+  #   
+  # end
   
 
   
