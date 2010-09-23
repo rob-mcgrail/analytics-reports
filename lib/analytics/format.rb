ffacde9571ef2e16f70fea38a23df942cb113de1
robomc <rob.mcgrail@gmail.com>
2010-09-23 18:40:01 +1200

    adding scruff.rb require

--- lib/analytics/format.rb
+++ lib/analytics/format.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + "/format/gchart.rb") #module
+require File.expand_path(File.dirname(__FILE__) + "/format/scruff.rb") #module
 
 require File.expand_path(File.dirname(__FILE__) + "/format/format.rb") #parent
 
