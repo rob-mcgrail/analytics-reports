244bc2c04d66bc43b7222f600b2871ddb937d979
robomc <rob.mcgrail@gmail.com>
2010-09-27 11:23:47 +1300

    created very basic keywords api class

---
+++ lib/analytics/api/keywords.rb
@@ -0,0 +1,58 @@
+def Keywords
+  
+  def initialize
+    @start_date_reporting = $periods.start_date_reporting
+    @end_date_reporting = $periods.end_date_reporting
+    @start_date_previous = $periods.start_date_previous
+    @end_date_previous = $periods.end_date_previous
+    @start_date_baseline = $periods.start_date_baseline
+    @end_date_baseline = $periods.end_date_baseline
+    @reporting_number_of_months = $periods.reporting_number_of_months
+    @baseline_number_of_months = $periods.baseline_number_of_months
+    
+    @all_results = ["You need to run my methods :|"]
+    
+    @arbitrary = nil
+    @reporting = nil
+    @previous = nil
+    @baseline = nil
+
+  end
+
+  def reporting( start_date = @start_date_reporting, 
+                    end_date = @end_date_reporting)
+  
+    report = Garb::Report.new($profile.garb,
+                              :start_date => start_date,
+                              :end_date => end_date,
+                              :limit => limit)
+  
+    if $profile.segment?
+      report.set_segment_id($profile.segment)
+    end
+  
+    report.metrics :visits
+    report.dimensions :keywords
+  
+    report.sort :visits.desc
+  
+  
+    rows = Array.new
+  
+    report.results.each do |thing|
+      a = Array.new
+      a << "#{thing.keywords}"
+      a << "#{thing.visits}"
+    
+      rows << a
+    end
+  
+    header = ["Keyword", "Visits"]
+
+    hash = { :title => "Search keywords", :table_id => "search_keywords", :header => header, :rows => rows}
+  
+    @in_results = OpenStruct.new(hash)
+    @in_results
+  end
+  
+end
