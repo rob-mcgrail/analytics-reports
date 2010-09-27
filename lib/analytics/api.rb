d999b3a7a0e5e04c86299ed6c7616cbf12eea422
robomc <rob.mcgrail@gmail.com>
2010-09-27 16:53:42 +1300

    added browser class to api requires

--- lib/analytics/api.rb
+++ lib/analytics/api.rb
@@ -13,5 +13,6 @@ require File.expand_path(File.dirname(__FILE__) + "/api/countrysource.rb")
 require File.expand_path(File.dirname(__FILE__) + "/api/navigation.rb")
 require File.expand_path(File.dirname(__FILE__) + "/api/length.rb")
 require File.expand_path(File.dirname(__FILE__) + "/api/keywords.rb")
+require File.expand_path(File.dirname(__FILE__) + "/api/browser.rb")
 
 
