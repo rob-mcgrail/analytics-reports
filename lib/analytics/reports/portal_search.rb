043919864cc1a0e85a60fbc2ce29cfa25030e7d4
robomc <rob.mcgrail@gmail.com>
2010-10-06 15:21:23 +1300

    portal search report

---
+++ lib/analytics/reports/portal_search.rb
@@ -0,0 +1,38 @@
+class PortalSearch < Report
+  
+  def initialize(name = "Portal search terms")
+    @name = name
+    @now = DateTime.now
+    
+    @dir = "portal_searches"
+    @path = File.expand_path(File.dirname(__FILE__) + "/../../../output/#{@dir}")
+    FileUtils.mkdir_p @path
+    @file = File.new(@path + "/#{@name.gsub(" ", "-").downcase}-#{@now.strftime("%d%m")}.csv",  "w+")
+    $path = @path #export path variable for formatting classes that need it
+  end
+  
+  def session_startup
+    $profile = Profile.new
+    $profile.string = "Portal"
+    $profile.garb = Garb::Profile.first('35630502')
+  end
+  
+  def all
+    self.header
+    self.queries
+  end
+  
+  def queries
+    searches = Searches.new
+    
+    searches.reporting.each do |result|
+      $format.values [result.page_path.to_s, result.pageviews.to_s]
+    end
+    
+  end
+  
+  def header
+    $format.header ["Query", "Times"]
+  end
+  
+end
