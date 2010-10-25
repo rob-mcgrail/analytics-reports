5efb2e938708361ba1397ab7840ea57b2375fb1f
robomc <rob.mcgrail@gmail.com>
2010-10-26 10:11:46 +1300

    added require for google visulr module

--- lib/analytics/format.rb
+++ lib/analytics/format.rb
@@ -1,5 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + "/format/gchart.rb") #module
-require File.expand_path(File.dirname(__FILE__) + "/format/scruff.rb") #module
+require File.expand_path(File.dirname(__FILE__) + "/format/gchart.rb")        #module
+require File.expand_path(File.dirname(__FILE__) + "/format/scruff.rb")        #module
+require File.expand_path(File.dirname(__FILE__) + "/format/googlevisulr.rb")  #module
+
 
 require File.expand_path(File.dirname(__FILE__) + "/format/format.rb") #parent
 
