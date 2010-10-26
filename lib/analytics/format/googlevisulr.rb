9dc6d89a4c9631c22ebb2d00fed5e838baabeddf
robomc <rob.mcgrail@gmail.com>
2010-10-26 13:16:26 +1300

    trying out line chart

--- lib/analytics/format/googlevisulr.rb
+++ lib/analytics/format/googlevisulr.rb
@@ -1,9 +1,9 @@
 module GVisualCharts
   require File.expand_path(File.dirname(__FILE__) + "../../../../plugins/winston-google_visualr/init.rb")
   
-  def GVisualCharts.large_linegraph(title, data1, data2, data3, data_title1, data_title2, data_title3, width = 600, height = 200)
+  def GVisualCharts.large_linegraph(title, data1, data2, data3, data_title1, data_title2, data_title3, width = 630, height = 200)
     
-    @chart = GoogleVisualr::AreaChart.new    
+    @chart = GoogleVisualr::LineChart.new    
 
     @chart.add_column('string', 'Date' )
     @chart.add_column('number', data_title1)
@@ -20,15 +20,15 @@ module GVisualCharts
 
     @chart.add_rows(data1.length)
     
-    
-    puts "data1:"
-    puts data1
-    
-    puts "data1 length:"
-    puts data1.length
-    
-    @chart.set_value(0, 0, $periods.start_date_reporting)
-    @chart.set_value(data1.length - 1, 0, $periods.end_date_reporting)
+    # @chart.set_value(0, 0, "#{$periods.end_date_reporting.strftime("%d/%m/%y")}")
+    # @chart.set_value(data1.length - 1, 0, "#{$periods.end_date_reporting.strftime("%d/%m/%y")}")
+
+    # i = 1
+    # 
+    # while i < (data1.length - 1)
+    #   @chart.set_value(i, 0, "")
+    #   i+=1
+    # end
 
     i = 0
     
@@ -53,7 +53,7 @@ module GVisualCharts
 
     @chart.width = width
     @chart.height = height
-
+    @chart.legend = "bottom"
                
     chart = @chart.render("#{title}")
     
